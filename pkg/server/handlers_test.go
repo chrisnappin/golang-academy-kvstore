4577c8f9038fb93c23f1c690a77605be5a164314
chrisnappin <chris@nappin.com>
2022-07-15 13:48:52 +0100

    Added remaining REST tests

--- pkg/server/handlers_test.go
+++ pkg/server/handlers_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http/httptest"
 	"regexp"
 	"store/pkg/kvstore"
+	"strings"
 	"testing"
 )
 
@@ -52,6 +53,188 @@ func TestGetPopulatedKey(t *testing.T) {
 	storeKey(recorder, request, "", store, testLogger)
 
 	checkResponse(t, recorder, 200, "123")
+
+	kvstore.Close(store)
+}
+
+func TestPutNoKeySpecified(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	request := httptest.NewRequest("PUT", "/store", nil) // no key specified
+	store := kvstore.NewKVStore()
+
+	storeKey(recorder, request, "", store, testLogger)
+
+	checkResponse(t, recorder, 400, "Bad Request")
+
+	kvstore.Close(store)
+}
+
+func TestPutNoOwnerSpecified(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	request := httptest.NewRequest("PUT", "/store/abc", nil)
+	store := kvstore.NewKVStore()
+
+	storeKey(recorder, request, "", store, testLogger) // no owner
+
+	checkResponse(t, recorder, 400, "Bad Request")
+
+	kvstore.Close(store)
+}
+
+func TestPutValid(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	request := httptest.NewRequest("PUT", "/store/abc", strings.NewReader("123"))
+	store := kvstore.NewKVStore()
+
+	storeKey(recorder, request, "user_a", store, testLogger)
+
+	checkResponse(t, recorder, 200, "OK")
+
+	value, present := kvstore.Read(store, "abc")
+	if !present {
+		t.Fatal("Key PUT didn't write to store")
+	}
+	if value != "123" {
+		t.Fatal("Invalid key value: ", value)
+	}
+
+	kvstore.Close(store)
+}
+
+func TestPutWrongOwner(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	request := httptest.NewRequest("PUT", "/store/abc", strings.NewReader("123"))
+	store := kvstore.NewKVStore()
+	kvstore.Write(store, "abc", "123", "user_b") // key already exists, owned by user_b
+
+	storeKey(recorder, request, "user_a", store, testLogger) // attempt to write by user_a
+
+	checkResponse(t, recorder, 403, "Forbidden\n")
+
+	kvstore.Close(store)
+}
+
+func TestDeleteNoKeySpecified(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	request := httptest.NewRequest("DELETE", "/store", nil) // no key specified
+	store := kvstore.NewKVStore()
+
+	storeKey(recorder, request, "", store, testLogger)
+
+	checkResponse(t, recorder, 400, "Bad Request")
+
+	kvstore.Close(store)
+}
+
+func TestDeleteNoOwnerSpecified(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	request := httptest.NewRequest("DELETE", "/store/abc", nil)
+	store := kvstore.NewKVStore()
+
+	storeKey(recorder, request, "", store, testLogger) // no owner
+
+	checkResponse(t, recorder, 400, "Bad Request")
+
+	kvstore.Close(store)
+}
+
+func TestDeleteNoSuchKey(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	request := httptest.NewRequest("DELETE", "/store/abc", nil)
+	store := kvstore.NewKVStore()
+	// key not in store
+
+	storeKey(recorder, request, "user_a", store, testLogger)
+
+	checkResponse(t, recorder, 404, "Not Found\n")
+
+	kvstore.Close(store)
+}
+
+func TestDeleteValid(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	request := httptest.NewRequest("DELETE", "/store/abc", nil)
+	store := kvstore.NewKVStore()
+	kvstore.Write(store, "abc", "123", "user_a")
+
+	storeKey(recorder, request, "user_a", store, testLogger)
+
+	checkResponse(t, recorder, 200, "OK")
+
+	_, present := kvstore.Read(store, "abc")
+	if present {
+		t.Fatal("Key DELETE didn't delete in store")
+	}
+
+	kvstore.Close(store)
+}
+
+func TestDeleteWrongUser(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	request := httptest.NewRequest("DELETE", "/store/abc", nil)
+	store := kvstore.NewKVStore()
+	kvstore.Write(store, "abc", "123", "user_a")
+
+	storeKey(recorder, request, "user_b", store, testLogger) // different user
+
+	checkResponse(t, recorder, 403, "Forbidden\n")
+
+	kvstore.Close(store)
+}
+
+func TestListNoKeySpecified(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	request := httptest.NewRequest("GET", "/list", nil) // no key specified
+	store := kvstore.NewKVStore()
+
+	listKey(recorder, request, "", store, testLogger)
+
+	checkResponse(t, recorder, 400, "Bad Request")
+
+	kvstore.Close(store)
+}
+
+func TestListNoSuchKey(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	request := httptest.NewRequest("GET", "/list/abc", nil)
+	store := kvstore.NewKVStore()
+	// no key populated
+
+	listKey(recorder, request, "user_a", store, testLogger)
+
+	checkResponse(t, recorder, 404, "Not Found\n")
+
+	kvstore.Close(store)
+}
+
+func TestListValid(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	request := httptest.NewRequest("GET", "/list/abc", nil)
+	store := kvstore.NewKVStore()
+	kvstore.Write(store, "abc", "123", "user_a")
+
+	listKey(recorder, request, "user_a", store, testLogger)
+
+	checkResponse(t, recorder, 200, `{"key":"abc","owner":"user_a","writes":1,"reads":0,"age":0}`)
+
+	kvstore.Close(store)
+}
+
+func TestListAllValid(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	request := httptest.NewRequest("GET", "/list", nil)
+	store := kvstore.NewKVStore()
+	kvstore.Write(store, "abc", "123", "user_a")
+	kvstore.Write(store, "def", "456", "user_b")
+	kvstore.Write(store, "ghi", "789", "user_a")
+
+	listAll(recorder, request, "user_a", store, testLogger)
+
+	checkResponse(t, recorder, 200, `[
+		{"key":"abc","owner":"user_a","writes":1,"reads":0,"age":0},
+		{"key":"def","owner":"user_b","writes":1,"reads":0,"age":0},
+		{"key":"ghi","owner":"user_a","writes":1,"reads":0,"age":0}
+	]`)
 
 	kvstore.Close(store)
 }
